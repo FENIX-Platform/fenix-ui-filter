7e236014295351c976f62ea210d10ee6936a4c5a
babic <Barbara.Cintoli@fao.org>
2017-07-12 10:49:49 +0200

    Beta 91

--- src/nls/labels.js
+++ src/nls/labels.js
@@ -1,14 +1,16 @@
 define([
         "./en/labels",
-        "./fr/labels"
+        "./fr/labels",
+        "./es/labels"
     ],
-    function (i18nEn, i18nFr) {
+    function (i18nEn, i18nFr, i18nEs) {
 
         'use strict';
 
         return {
 
             en: i18nEn,
-            fr: i18nFr
+            fr: i18nFr,
+            es: i18nEs
         }
     });
