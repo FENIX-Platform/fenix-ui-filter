166030d791078f4f6087f63bc9b65ed247dec774
babic <Barbara.Cintoli@fao.org>
2017-12-21 16:44:30 +0100

    Version beta 96

---
+++ src/nls/es/labels.js
@@ -0,0 +1,16 @@
+define(function () {
+
+    'use strict';
+
+    return {
+
+        enable_filter : "Activar filtro",
+        disable_filter : "Desactivar filtro",
+        clear_all_btn: "Resetear Todo",
+        select_all_btn: "Seleccionar Todo",
+        search_placeholder: "Filtrar",
+
+        tooltip_remove_selector: "Quitar seleccionador"
+
+    }
+});
